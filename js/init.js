9779d26d7864cd777f3a7224d0dfc91a3f289a4c
prostonastya <find@li.ru>
2017-01-29 14:39:39 +0200

    проект без последних правок

---
+++ js/init.js
@@ -0,0 +1,11 @@
+  ;(function ($){
+  		// init bpopup
+  $('._js-btn-open-modal').on('click', function (){
+  	$('#modal-order').bPopup({
+  		closeClass: 'modal-close',
+  	});
+  });
+  // init formstyler
+  $('select').styler();
+  })(jQuery);
+  
